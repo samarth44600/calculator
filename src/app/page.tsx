3960c1e38c6518eeb026e009d7946b4f48e14fdc
Samarth Subedi <subedi42samarth@gmail.com>
2023-04-28 17:45:22 +0545

    UI completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.scss";
+import Keypad from "@/components/Keypad/Keypad";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main className={styles.mainDiv}>
-
-  </main>;
+  return (
+    <main className={styles.mainDiv}>
+      <Keypad />
+    </main>
+  );
 }
---
+++ src/components/Keypad/Keypad.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./Keypad.module.scss";
+import { HiBackspace } from "react-icons/hi";
+type Props = {};
+
+const Keypad = (props: Props) => {
+  return (
+    <div className={styles.keypadDiv}>
+      <div className={styles.keypadRow}>
+        <button className={styles.keypadButton}>AC</button>
+        <button className={styles.keypadButton}>%</button>
+        <button className={styles.keypadButton}>/</button>
+        <button className={styles.keypadButton}>
+          <HiBackspace />
+        </button>
+      </div>
+      <div className={styles.keypadRow}>
+        <button className={styles.keypadButton}>7</button>
+        <button className={styles.keypadButton}>8</button>
+        <button className={styles.keypadButton}>9</button>
+        <button className={styles.keypadButton}>*</button>
+      </div>
+      <div className={styles.keypadRow}>
+        <button className={styles.keypadButton}>4</button>
+        <button className={styles.keypadButton}>5</button>
+        <button className={styles.keypadButton}>6</button>
+        <button className={styles.keypadButton}>-</button>
+      </div>
+      <div className={styles.keypadRow}>
+        <button className={styles.keypadButton}>1</button>
+        <button className={styles.keypadButton}>2</button>
+        <button className={styles.keypadButton}>3</button>
+        <button className={styles.keypadButton}>+</button>
+      </div>
+      <div className={styles.keypadRow}>
+        <button className={styles.keypadButton}>0</button>
+        <button className={styles.keypadButton}>.</button>
+        <button className={styles.keypadButton}>=</button>
+      </div>
+    </div>
+  );
+};
+
+export default Keypad;
